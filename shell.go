af99f62496cc6ab6bb33328ba728995f3925f879
richard <richardthomas215@yahoo.com>
2019-03-20 10:50:34 -0400

    implemented scanner UI

---
+++ shell.go
@@ -0,0 +1,20 @@
+package main
+
+import ("fmt"
+		"bufio"
+		"os")
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+ 	 fmt.Print("Enter Command? ")
+
+	 scanner.Scan()
+	 
+	fmt.Print( scanner.Text(), "\n" )
+
+
+
+
+}

42d43844fdaa9e513fab559be6a79c9d3b17e84f
Richard Thomas <richardthomas215@yahoo.com>
2019-06-18 21:26:22 -0400

    modified function toreturn a string of working directory instead of printing

--- pwd.go
+++ pwd.go
@@ -1,18 +1,17 @@
-
 package main
 
-import(
-	"os" 
-	"fmt"
+import (
 	"log"
-  )
-  
-  func pwd() {
+	"os"
+)
+
+func pwd() string {
 
 	dir, err := os.Getwd()
-	  if err != nil {
-		  log.Fatal(err)
-	  }
-	fmt.Println(dir)
-	
-  }
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dir
+}

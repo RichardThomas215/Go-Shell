d5858dd71cff77169b31004caaae72df2056eaf7
Richard Thomas <richardthomas215@yahoo.com>
2019-06-23 08:30:32 -0400

    edited zip file name and cleaned up comments

--- zip.go
+++ zip.go
@@ -1,44 +1,42 @@
 package main
 
 import (
-    "archive/zip"
-    "log"
-    "os"
-    "io/ioutil"
+	"archive/zip"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
-func zipUp(filesToArchive  [] string) {
-    // Create a file to write the archive buffer to
-    // Could also use an in memory buffer.
-    outFile, err := os.Create("test.zip")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer outFile.Close()
+func zipUp(filesToArchive []string) {
 
-    // Create a zip writer on top of the file writer
-    zipWriter := zip.NewWriter(outFile)
+	// Create a file to write the archive buffer to
+	outFile, err := os.Create("files.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outFile.Close()
 
-	// Create and write files to the archive, which in turn
-    // are getting written to the underlying writer to the
-    // .zip file we created at the beginning
-    for i := 1 ; i< len(filesToArchive); i++ {
+	// Create a zip writer on top of the file writer
+	zipWriter := zip.NewWriter(outFile)
 
-            fileWriter, err := zipWriter.Create(filesToArchive[i])
-            if err != nil {
-                    log.Fatal(err)
-			}
-            data, err:= ioutil.ReadFile(filesToArchive[i])
-            
-               _, err = fileWriter.Write([]byte( data))
-              if err != nil {
-                      log.Fatal(err)
-               }
-    }
+	// Create and write files to the archive
+	for i := 1; i < len(filesToArchive); i++ {
 
-    // Clean up
-    err = zipWriter.Close()
-    if err != nil {
-            log.Fatal(err)
-    }
+		fileWriter, err := zipWriter.Create(filesToArchive[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		data, err := ioutil.ReadFile(filesToArchive[i])
+
+		_, err = fileWriter.Write([]byte(data))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Clean up
+	err = zipWriter.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
